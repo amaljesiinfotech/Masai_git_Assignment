9fe954f4a23a407d69f87e1d423ac74133ac60d8
amalajesima <amaljesiinfotech@gmail.com>
2026-06-08 10:21:49 +0530

    added new feature

--- util.py
+++ util.py
@@ -2,5 +2,7 @@ l=[10,20,30,40,50]
 for i in l:
     if i>35:
         print("Given number is >35:", i)
+    elif i==35:
+        print("Given number is equal to 35:",i)
     else:
         print("Given number is not greater than 35:",i)
